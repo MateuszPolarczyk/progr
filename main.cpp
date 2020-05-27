c550e7209bedd932b5ccd8d86d815898a7c0036f
Mateusz <59296230+MateuszCode@users.noreply.github.com>
2020-05-27 10:17:34 +0200

    Wypisanie zdań linia po lini

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int linie = 0;
+string x;
+
+int main(){
+	ifstream plik;
+	plik.open("C:\\Users\\Mateusz\\a.txt");
+	if(plik.good()){
+		while(!plik.eof()){
+			plik >> x;
+			cout<<linie++<<": "<<x<<"\n";
+		}
+	}
+	string t[linie];
+	linie = 0;
+	if(plik.good()){
+		while(!plik.eof()){
+			linie++;
+			plik >> x;
+			t[linie] = x;
+			cout<<t[linie];
+		}
+	}	
+	plik.close();
+	
+	return 0;
+}
