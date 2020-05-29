ce231bde5baa588c720609a0e328e82183d0ec0f
Mateusz <59296230+MateuszCode@users.noreply.github.com>
2020-05-29 08:40:53 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+ofstream output;
+
+void wypiszLiczbeLosowa(int max){
+	int a;
+	a = rand()%max;
+	cout << a;
+}
+
+void wypiszLiczbeJSON(int liczba){
+	int b;
+	b=rand()%liczba+1;
+	output << "[\n{\n \"liczba:\": " << b << "\n}\n]";
+}
+
+int main(int argc, char** argv) {
+	
+	output.open("output.txt");
+	
+	cout << "Wczytaj liczbe losowa: ";
+	cin >> max;
+	cout << "Wczytaj liczbe losowa JSON: ";
+	cin >> liczba;
+	
+	wypiszLiczbeLosowa(max);
+	wypiszLiczbeJSON(liczba);
+	
+	output.close();
+	return 0;
+}
