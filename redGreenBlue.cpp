123aaf20c70c0d71903386c9867671cb61c12169
Mateusz <59296230+MateuszCode@users.noreply.github.com>
2020-11-18 12:05:44 +0100

    Wyłuskiwanie kolorów

--- redGreenBlue.cpp
+++ redGreenBlue.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main(){
 	int kolor = 0x123456;
 	cout << "RED: " << hex << ((kolor & 0x00ff0000) >> 16) << "\n"
-		 << "GREEN: " << hex << ((kolor & 0x0000ff00) >> 8) << "\n"
-		 << "BLUE: " << hex << ((kolor & 0x000000ff) >> 0);
+	     << "GREEN: " << hex << ((kolor & 0x0000ff00) >> 8) << "\n"
+     	     << "BLUE: " << hex << ((kolor & 0x000000ff) >> 0);
 	
 	return 0;
 }
